84152518cca1c314b03fc5103f6c84c136efcc08
Mushi <144794503+Mouche00@users.noreply.github.com>
2024-10-31 14:36:48 +0100

    [JBP-35] feature: create pigeon and breeder entities

---
+++ src/main/java/org/ioc/jb8pigeonskyrace/models/Breeder.java
@@ -0,0 +1,29 @@
+package org.ioc.jb8pigeonskyrace.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Document(collation = "breeders")
+public class Breeder {
+    @Id
+    private String id;
+
+    private String username;
+    private String password;
+    private String loftName;
+    private double loftLatitude;
+    private double loftLongitude;
+    private double finalScore;
+
+    @DBRef
+    private List<Pigeon> pigeons;
+}
---
+++ src/main/java/org/ioc/jb8pigeonskyrace/utils/enums/Gender.java
@@ -0,0 +1,6 @@
+package org.ioc.jb8pigeonskyrace.utils.enums;
+
+public enum Gender {
+    FEMALE,
+    MALE
+}
