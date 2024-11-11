db684aa30b3f7b0743fe7f39b8407cc77cf7eb23
Mouche00 <144794503+Mouche00@users.noreply.github.com>
2024-11-11 10:25:03 +0100

    Revert "Feature/jbp 19/breeder registration login"

--- src/main/java/org/ioc/jb8pigeonskyrace/exception/ResponseNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package org.ioc.jb8pigeonskyrace.exception;
-
-public class ResponseNotFoundException extends RuntimeException {
-    public ResponseNotFoundException(String message) {
-        super(message);
-    }
-}
--- src/main/java/org/ioc/jb8pigeonskyrace/services/BreederAuthService.java
+++
@@ -1,8 +0,0 @@
-package org.ioc.jb8pigeonskyrace.services;
-
-import org.ioc.jb8pigeonskyrace.models.Breeder;
-
-public interface BreederAuthService {
-    public String register(Breeder breeder);
-    public boolean login(String username, String password);
-}
