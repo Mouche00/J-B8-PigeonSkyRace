9bc2ea69441c7be3fc10813b9c257b3d1df76254
Mushi <144794503+Mouche00@users.noreply.github.com>
2024-11-13 14:58:18 +0100

    refactor: implement coordinates object

--- src/main/java/org/ioc/jb8pigeonskyrace/models/Race.java
+++ src/main/java/org/ioc/jb8pigeonskyrace/models/Race.java
@@ -1,6 +1,7 @@
 package org.ioc.jb8pigeonskyrace.models;
 
 import lombok.Data;
+import org.ioc.jb8pigeonskyrace.utils.Coordinates;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -16,8 +17,7 @@ public class Race {
     @Id
     private String id;
     private String name;
-    private double latitude;
-    private double longitude;
+    private Coordinates startCoordinates;
     private LocalDateTime startDate;
     private double avgDistance;
     private LocalDateTime closedAt;
---
+++ src/main/java/org/ioc/jb8pigeonskyrace/utils/annotations/RefExistsValidator.java
@@ -0,0 +1,20 @@
+//package org.ioc.jb8pigeonskyrace.utils.annotations;
+//
+//import jakarta.validation.ConstraintValidator;
+//import jakarta.validation.ConstraintValidatorContext;
+//import org.springframework.beans.factory.annotation.Autowired;
+//
+//public class RefExistsValidator implements ConstraintValidator<RefExists, String> {
+//    @Autowired
+//    private
+//
+//    @Override
+//    public void initialize(RefExists constraintAnnotation) {
+//        ConstraintValidator.super.initialize(constraintAnnotation);
+//    }
+//
+//    @Override
+//    public boolean isValid(String s, ConstraintValidatorContext constraintValidatorContext) {
+//        return false;
+//    }
+//}
